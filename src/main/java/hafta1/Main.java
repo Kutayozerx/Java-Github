220aa0d1867ee28a0572108e0512ff3849722bf4
Kutayozer17 <kutayozer04@gmail.com>
2025-02-25 15:09:30 +0300

    Initial commit

---
+++ src/main/java/hafta1/Main.java
@@ -0,0 +1,20 @@
+package hafta1;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("Hafta1");
+
+        int x = 20;
+        int y = 40;
+
+        boolean işlem = x >10;
+        boolean işlem1 = x+y > 100;
+        boolean işlem2 = y % x < 120;
+
+
+        System.out.println(işlem);
+        System.out.println(işlem1);
+        System.out.println(işlem2);
+    }
+}
