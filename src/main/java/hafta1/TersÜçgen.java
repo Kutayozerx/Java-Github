90dcc2e79e2e73903e8cb7d3cf811f5f788170ea
Kutayozer17 <kutayozer04@gmail.com>
2025-03-06 14:28:36 +0300

    ödev

--- src/main/java/hafta1/TersÜçgen.java
+++ src/main/java/hafta1/TersÜçgen.java
@@ -1,11 +1,21 @@
 package hafta1;
 
+import java.util.Scanner;
+
 public class TersÜçgen {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+
+        System.out.println("Ters Üçgenin Basamak sayısını giriniz: ");
+        number = scanner.nextInt();
+
+
         //Ters Üçgen için döngü
-        for (int i = 10; i >= 1; i--) {
+        for (int i = number; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
